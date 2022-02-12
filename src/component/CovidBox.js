9d1c6993c18b58aff4b2705b77e675667d2d1d68
Rabindranath Mondal <connectrnmwd@gmail.com>
2022-02-12 09:30:51 +0530

    Covid Live tracker created and updated

--- src/component/CovidBox.js
+++ src/component/CovidBox.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CovidBoxItem from "../component/CovidBoxItem";
 
 const CovidBox = () => {
+  const [data, setData] = useState([]);
+
+  const getCovidData = async () => {
+    try {
+      const res = await fetch("https://data.covid19india.org/data.json");
+      const actualData = await res.json();
+      // console.log(actualData.statewise[0]);
+      setData(actualData.statewise[0]);
+    } catch (err) {
+      alert("This is some error");
+    }
+  };
+  useEffect(() => {
+    getCovidData();
+  }, []);
   return (
     <>
       <div className="covid-news-item row align-items-center mb-3">
         <CovidBoxItem subtitle="Our" title="Country" heading="india" />
-        <CovidBoxItem subtitle="Total" title="Recovered" heading="20073367" />
-        <CovidBoxItem subtitle="Total" title="Confirmed" heading="24046120" />
-        <CovidBoxItem subtitle="Total" title="Death" heading="262350" />
-        <CovidBoxItem subtitle="Total" title="Active" heading="3700327" />
-        <CovidBoxItem subtitle="On" title="Upadated" heading="10/02/2022" />
+        <CovidBoxItem
+          subtitle="Total"
+          title="Recovered"
+          heading={data.recovered}
+        />
+        <CovidBoxItem
+          subtitle="Total"
+          title="Confirmed"
+          heading={data.confirmed}
+        />
+        <CovidBoxItem subtitle="Total" title="Death" heading={data.deaths} />
+        <CovidBoxItem subtitle="Total" title="Active" heading={data.active} />
+        <CovidBoxItem
+          subtitle="Last"
+          title="Upadated"
+          heading={data.lastupdatedtime}
+        />
       </div>
     </>
   );
